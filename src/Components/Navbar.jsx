969199a810812b1bf6189a5468938892f7492741
Humaira Islam <ihumaira.webfront@gmail.com>
2024-09-11 23:37:44 +0600

    navbar complete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
 
+import { CiSearch } from "react-icons/ci";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { PiHandbag } from "react-icons/pi";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+         <nav className="border-b border-gray-200">
+      <div className="container flex justify-between items-center py-4">
+        {/* Logo */}
+        <div className="flex items-center space-x-1">
+          <img src="/images/logo.png" alt="" />
+        </div>
+
+        {/* Menu Links */}
+        <div className="flex space-x-8">
+          <a href="/" className="text-pink-500 hover:text-black font-semibold">HOME</a>
+          <a href="/about" className="text-black hover:text-pink-500 font-semibold">ABOUT</a>
+          <a href="/services" className="text-black hover:text-pink-500 font-semibold">SERVICES</a>
+          <a href="/shop" className="text-black hover:text-pink-500 font-semibold">SHOP</a>
+          <a href="/pages" className="text-black hover:text-pink-500 font-semibold">PAGES</a>
+          <a href="/blog" className="text-black hover:text-pink-500 font-semibold">BLOG</a>
+          <a href="/contact" className="text-black hover:text-pink-500 font-semibold">CONTACT</a>
+        </div>
+
+        {/* Icons */}
+        <div className="flex items-center space-x-6 text-[25px]">
+          {/* Search Icon */}
+          <CiSearch className=''/>
+          {/* Heart Icon with Item Count */}
+          <IoMdHeartEmpty />         
+          {/* Cart Icon with Item Count */}
+          <PiHandbag />       
+
+          {/* Cart Total */}
+          
+        </div>
+      </div>
+    </nav>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,  // Center the container by default
+        padding: '1rem',  // Add padding around the container
+        screens: {
+          sm: '100%',     // Full width on small screens
+          md: '728px',    // Custom width on medium screens
+          lg: '1024px',   // Custom width on large screens
+          xl: '1280px',   // Custom width on extra large screens
+          '2xl': '1440px', // Custom width on 2XL screens
+        },
+      },
+    },
   },
   plugins: [],
 }
