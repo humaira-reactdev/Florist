35809beed4e832f3ba87670097509a676ea64a30
sonia1115 <152697208+sonia1115@users.noreply.github.com>
2024-10-01 20:24:51 +0600

    checkoutPage done

--- src/App.jsx
+++ src/App.jsx
@@ -13,28 +13,28 @@ import { ToastContainer } from 'react-toastify'
 import DetailsPage from './Pages/DetailsPage'
 import ReviewPage from './Pages/ReviewPage'
 import ShippingPage from './Pages/shippingPage'
+import CheckoutPage from './Pages/CheckoutPage'
 
 function App() {
 
   const route= createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        <Route path='/' element={<LayoutOne/>}>
-          <Route index element = {<Homepage/>}/>
-          <Route path='/contact' element={<ContactPage/>}/>
-          <Route path='/shop' element={<ShopPage/>}/>
-          <Route path='/about' element={<AboutPage/>}/>
-          <Route path='/services' element={<ServicesPage/>}/>
-          <Route path='/cart' element={<CartPage/>}/>
-          <Route path='/details' element={<DetailsPage/>}/>
-          <Route path='/review'  element={<ReviewPage/>}/>
-          <Route path='/shipping' element={<ShippingPage/>}/>
+        <Route path='/'           element={<LayoutOne/>}>
+          <Route index                element= {<Homepage/>}/>
+          <Route path='/contact'      element={<ContactPage/>}/>
+          <Route path='/shop'         element={<ShopPage/>}/>
+          <Route path='/about'        element={<AboutPage/>}/>
+          <Route path='/services'     element={<ServicesPage/>}/>
+          <Route path='/cart'         element={<CartPage/>}/>
+          <Route path='/details'      element={<DetailsPage/>}/>
+          <Route path='/review'       element={<ReviewPage/>}/>
+          <Route path='/shipping'     element={<ShippingPage/>}/>
+          <Route path='/checkout'     element={<CheckoutPage/>}/>
         </Route>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/signup' element={<SignupPage/>}/>
+        <Route path='/login'      element={<LoginPage/>}/>
+        <Route path='/signup'     element={<SignupPage/>}/>
         <Route path='/forgotpass' element={<ForgotPasswordPage/>}/>
-        
-        
       </Route>
     )
   )
